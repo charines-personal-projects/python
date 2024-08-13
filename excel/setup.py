59cfa0790f1cb33b8915744d624edf3dc0b07468
Charles Rodrigues <44099191+charines@users.noreply.github.com>
2024-08-13 08:34:26 -0300

    Update setup.py

--- excel/setup.py
+++ excel/setup.py
@@ -6,7 +6,7 @@ setup(
     description='Projeto para filtrar registros de e-mail em planilhas Excel',
     author='Charles',
     author_email='charles.rodrigues@dsop.com.br',
-    url='https://github.com/seu_usuario/excel_data_filter',
+    url='https://github.com/charines-personal-projects/rpa.git',
     packages=find_packages(),  # Procurar pacotes na raiz do projeto
     install_requires=[
         'pandas',    # Biblioteca necessária para manipulação de dados
